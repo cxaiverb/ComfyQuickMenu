70ae581f6931db19abcdfad4555e8cd382a67113
xMiraiChan <xMiraiChan@protonmail.com>
2021-11-28 16:03:05 -0500

    project refactoring

--- ComfyQM.cs
+++ ComfyQM.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 using UnityEngine.XR;
 using VRC.UI.Core;
 
+[assembly: MelonInfo(typeof(ComfyQM_Standalone.ComfyQM), "ComfyQM", "1.2.0", "xaiver")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
 namespace ComfyQM_Standalone
 {
     public class ComfyQM : MelonMod
@@ -17,65 +20,46 @@ namespace ComfyQM_Standalone
 
         public IEnumerator WaitForUIMan()
         {
-            while (VRCUiManager.field_Private_Static_VRCUiManager_0 == null) yield return null;
-            while (UIManager.field_Private_Static_UIManager_0 == null) yield return null;
+            while (VRCUiManager.field_Private_Static_VRCUiManager_0 is null) yield return null;
+            while (UIManager.field_Private_Static_UIManager_0 is null) yield return null;
             while (GameObject.Find("UserInterface").GetComponentInChildren<VRC.UI.Elements.QuickMenu>(true) == null) yield return null;
-            QuickMenuObject = GameObject.Find("UserInterface").GetComponentInChildren<VRC.UI.Elements.QuickMenu>(true).gameObject;
-            OnUIManInit();
-        }
 
+            QuickMenuObject = GameObject.Find("UserInterface").GetComponentInChildren<VRC.UI.Elements.QuickMenu>(true).gameObject;
+            LeftHand = VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.transform.Find("DotLeftHand").gameObject;
+            RightHand = VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.transform.Find("DotRightHand").gameObject;
+        }
 
         public override void OnApplicationStart()
         {
             ComfyQuickMenu = MelonPreferences.CreateCategory("ComfyQM");
             ComfyToggle = ComfyQuickMenu.CreateEntry("ComfyQM Toggle", false);
 
+            if (!XRDevice.isPresent) return;
+
             MelonCoroutines.Start(WaitForUIMan());
-            
-            HarmonyInstance.Patch(typeof(VRC.UI.Elements.QuickMenu).GetMethod(nameof(VRC.UI.Elements.QuickMenu.Method_Private_Boolean_0)),
-                typeof(ComfyQM).GetMethod(nameof(IsAttachedToHandPatch), BindingFlags.NonPublic | BindingFlags.Static).ToNewHarmonyMethod());
-            HarmonyInstance.Patch(typeof(VRC.UI.Elements.QuickMenu).GetMethod(nameof(VRC.UI.Elements.QuickMenu.Method_Private_Boolean_1)),
-                typeof(ComfyQM).GetMethod(nameof(IsAttachedToHandPatch), BindingFlags.NonPublic | BindingFlags.Static).ToNewHarmonyMethod());
 
-        }
+            HarmonyInstance.Patch(
+                typeof(VRC.UI.Elements.QuickMenu).GetMethod(nameof(VRC.UI.Elements.QuickMenu.Method_Private_Boolean_0)),
+                typeof(ComfyQM).GetMethod(nameof(IsAttachedToHandPatch), BindingFlags.NonPublic | BindingFlags.Static).ToNewHarmonyMethod()
+            );
 
-        public void OnUIManInit()
-        {
-            LeftHand = VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.transform.Find("DotLeftHand").gameObject;
-            RightHand = VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.transform.Find("DotRightHand").gameObject;
+            HarmonyInstance.Patch(
+                typeof(VRC.UI.Elements.QuickMenu).GetMethod(nameof(VRC.UI.Elements.QuickMenu.Method_Private_Boolean_1)),
+                typeof(ComfyQM).GetMethod(nameof(IsAttachedToHandPatch), BindingFlags.NonPublic | BindingFlags.Static).ToNewHarmonyMethod()
+            );
         }
 
         public override void OnUpdate()
-        { 
-            if (LeftHand == null || RightHand == null)
-            {
+        {
+            if (!QuickMenuObject.active || !ComfyToggle.Value)
                 return;
-            }
-            if (!QuickMenuObject.active)
-            {
-                return;
-            }
-            if (ComfyToggle.Value == true)
-            {
-                LeftHand.SetActive(true);
-                RightHand.SetActive(true);
-                VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.field_Private_Boolean_2 = false;
-                VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.field_Private_Boolean_7 = false;
-            }
+
+            LeftHand?.SetActive(true);
+            RightHand?.SetActive(true);
+            VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.field_Private_Boolean_2 = false;
+            VRCUiCursorManager.field_Private_Static_VRCUiCursorManager_0.field_Private_Boolean_7 = false;
         }
 
-        private static bool IsAttachedToHandPatch(ref bool __result)
-        {
-            if (XRDevice.isPresent == false)
-            {
-                return true;
-            }
-            if (ComfyToggle.Value)
-            {
-                __result = false;
-                return false;
-            }
-            return true;
-        }
+        private static bool IsAttachedToHandPatch(ref bool __result) => !ComfyToggle.Value || (__result = false);
     }
 }
